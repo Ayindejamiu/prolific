f3c963026714c1a683720d320883ab69f4a4c638
jamiuayinde <ayindejamiu90@gmail.com>
2023-09-04 17:23:19 +0100

    this is urlss

--- app.js
+++
@@ -1,7 +0,0 @@
-// app.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ContactForm from './ContactForm'; // Assuming the ContactForm component is in the same directory
-
-ReactDOM.render(<ContactForm />, document.getElementById('root'));
